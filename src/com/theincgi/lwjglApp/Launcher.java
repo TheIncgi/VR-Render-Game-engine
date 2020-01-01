5c3c1c70feecf84bc2535c3e3b5926fe8d311fd0
TheIncgi <theincgi@gmail.com>
2020-01-01 05:17:44 -0500

    Added changes not included
    Launcher and DemoScene

--- src/com/theincgi/lwjglApp/Launcher.java
+++ src/com/theincgi/lwjglApp/Launcher.java
@@ -5,26 +5,34 @@ import org.lwjgl.glfw.GLFWErrorCallback;
 
 
 import com.theincgi.lwjglApp.misc.Logger;
+import com.theincgi.lwjglApp.render.vr.VRUtil;
 import com.theincgi.lwjglApp.scenes.DemoScene;
+import com.theincgi.lwjglApp.ui.AWindow;
+import com.theincgi.lwjglApp.ui.VRWindow;
 import com.theincgi.lwjglApp.ui.Window;
 
 public class Launcher {
 	public static final String APPLICATION_NAME = "Unnamed Application";
-	private static Window mainWindow;
+	private static AWindow mainWindow;
 	static Logger log = Logger.preferedLogger;
-	
+
+	static VRUtil vrUtil;
 	public static void main(String[] args) {
-		log.i("#main", "Main thread: "+Thread.currentThread().getId());
-		mainWindow = new Window(720, 440, APPLICATION_NAME, null);
-		mainWindow.setScene(new DemoScene(mainWindow));
-		GLFW.glfwSetErrorCallback((err, desc)->{
-			log.w("GL_ERROR", "["+Integer.toHexString(err)+ "]: "+ GLFWErrorCallback.getDescription(desc));
-		});
-		
-		mainWindow.show();
+		try(VRUtil util = new VRUtil()){
+			vrUtil = util;
+			util.initVR();
+			log.i("#main", "Main thread: "+Thread.currentThread().getId());
+			mainWindow = new VRWindow(util.getWidth(), util.getHeight(), APPLICATION_NAME, null);
+			mainWindow.setScene(new DemoScene(mainWindow));
+			GLFW.glfwSetErrorCallback((err, desc)->{
+				log.w("GL_ERROR", "["+Integer.toHexString(err)+ "]: "+ GLFWErrorCallback.getDescription(desc));
+			});
+
+			mainWindow.show();
+		}
 	}
 
-	public static Window getMainWindow() {
+	public static AWindow getMainWindow() {
 		return mainWindow;
 	}
 }
--- src/com/theincgi/lwjglApp/scenes/DemoScene.java
+++ src/com/theincgi/lwjglApp/scenes/DemoScene.java
@@ -2,25 +2,17 @@ package com.theincgi.lwjglApp.scenes;
 
 import java.io.File;
 import java.util.Optional;
-
-import org.lwjgl.util.vector.Matrix4f;
-
 import com.theincgi.lwjglApp.misc.Logger;
-import com.theincgi.lwjglApp.misc.MatrixStack;
-import com.theincgi.lwjglApp.mvc.view.drawables.HelloElements;
-import com.theincgi.lwjglApp.mvc.view.drawables.HelloTriangle;
 import com.theincgi.lwjglApp.render.Camera;
 import com.theincgi.lwjglApp.render.Drawable;
-import com.theincgi.lwjglApp.render.Location;
 import com.theincgi.lwjglApp.render.Model;
 import com.theincgi.lwjglApp.render.ObjManager;
 import com.theincgi.lwjglApp.render.shaders.ShaderManager;
-import com.theincgi.lwjglApp.render.shaders.ShaderProgram;
+import com.theincgi.lwjglApp.ui.AWindow;
 import com.theincgi.lwjglApp.ui.CallbackListener;
 import com.theincgi.lwjglApp.ui.Scene;
 import com.theincgi.lwjglApp.ui.Window;
 
-import static org.lwjgl.opengl.GL45.*;
 
 public class DemoScene extends Scene{
 	
@@ -28,7 +20,7 @@ public class DemoScene extends Scene{
 	Optional<Model> cube;
 	
 	
-	public DemoScene(Window window) {
+	public DemoScene(AWindow window) {
 		super(window);
 		sceneListener = Optional.of(new SceneCallbackListener());
 		cube = ObjManager.INSTANCE.get(new File("cmodels/softCube/softCube.obj"));
@@ -40,8 +32,8 @@ public class DemoScene extends Scene{
 	}
 	
 	@Override
-	public void render(double mouseX, double mouseY) {
-		super.render(mouseX, mouseY);
+	public void render(Camera camera, double mouseX, double mouseY) {
+		super.render(camera, mouseX, mouseY);
 		
 		cube.ifPresent(c->{
 			c.getLocation().rotate(.0052f, .0053334f, .0002f);
@@ -53,7 +45,7 @@ public class DemoScene extends Scene{
 	implements CallbackListener.OnMouseButton {
 
 		@Override
-		public boolean onMouseButton(Window window, double x, double y, int button, int action, int mods) {
+		public boolean onMouseButton(AWindow window, double x, double y, int button, int action, int mods) {
 			System.out.printf("MOUSE BUTTON: Pos: <%6.2f, %6.2f> |Button: button | Window: %s\n", x, y, button, window);
 			return true;
 		}
