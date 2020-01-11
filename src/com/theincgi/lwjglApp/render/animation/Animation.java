b49a2b7d410584e37722fe899a9e4beeb1b5c4b7
TheIncgi <theincgi@gmail.com>
2020-01-11 07:39:11 -0500

    Animation updates
    added some built in interpolators
    added pause resume support
    added animation chaining

--- src/com/theincgi/lwjglApp/render/animation/Animation.java
+++ src/com/theincgi/lwjglApp/render/animation/Animation.java
@@ -3,18 +3,18 @@ package com.theincgi.lwjglApp.render.animation;
 import java.util.ArrayList;
 
 public class Animation {
-	
+
 	private Updater<?>[] updaters;
 	private Long startTime;
 	private long duration;
 	private ArrayList<AnimationEventHandler> handlers;
 	private boolean reverse;
-	
-	
+	private Float progress;
+
 	public Animation(Updater<?>...updaters) {
 		this.updaters = updaters;
 	}
-	
+
 	public synchronized void addListener(AnimationEventHandler handler) {
 		if(handlers==null) handlers = new ArrayList<>();
 		handlers.add(handler);
@@ -24,31 +24,80 @@ public class Animation {
 			animationEventHandler.handleEvent(event);
 		}
 	}
-	
+
+	/**
+	 * Sets the <code>after</code> param to start when this animation competes forward<br>
+	 * This animation will be played if the after animation is played in reverse 
+	 * @param after
+	 * @return after argument
+	 * */
+	public synchronized Animation link(final Animation after) {
+		after.addListener(e->{
+			if(e.isFinRev())
+				play();
+		});
+		this.addListener(e->{
+			if(e.isFinFow())
+				after.play();
+		});
+		return after;
+	}
+
 	/**Does nothing if playing*/
 	public void setDuration(long milliseconds) {setDuration(milliseconds, TimeUnit.MILLISECONDS);}
 	/**Does nothing if playing*/
-	public void setDuration(long amount, TimeUnit unit) {
+	public synchronized void setDuration(long amount, TimeUnit unit) {
 		if(startTime==null)
 			duration = amount * unit.mult;
 	}
-	
+
+	/**Does from progress left off if paused*/
 	public synchronized void play() {
-		startTime = System.currentTimeMillis();
-		reverse = false;
-		notifyListeners(AnimationEvent.START_FORWARD);
+		if(progress!=null)
+			resume();
+		else {
+			startTime = System.currentTimeMillis();
+			reverse = false;
+			notifyListeners(AnimationEvent.START_FORWARD);
+		}
 	}
+	/**Does from progress left off if paused*/
 	public synchronized void playReverse() {
-		startTime = System.currentTimeMillis();
-		reverse = true;
-		notifyListeners(AnimationEvent.START_REVERSE);
+		if(progress!=null)
+			resume();
+		else {
+			startTime = System.currentTimeMillis();
+			reverse = true;
+			notifyListeners(AnimationEvent.START_REVERSE);
+		}
 	}
+	/**You can change values for duration while paused if needed, will resume based on % complete*/
+	public synchronized void pause() {
+		progress = getProgress();
+		startTime = null;
+	}
+	/**You can change values for duration while paused if needed, will resume based on % complete*/
+	public synchronized void resume() {
+		long now = System.currentTimeMillis();
+		long passed = (long) (duration * progress);
+		startTime = now-passed;
+		progress = null;
+	}
+	public synchronized boolean isPaused() {
+		return progress!=null;
+	}
+	/**Clear pause status to play from start or end*/
+	public synchronized Animation reset() {
+		progress = null; //mhmm, productive
+		return this;
+	}
+
 	public synchronized boolean isPlaying() {
 		return startTime!=null;
 	}
-	public void update() {
+	public synchronized void update() {
 		if(startTime==null) return;
-		float x = Math.min(1, (System.currentTimeMillis()-startTime) / (float)duration );
+		float x = getProgress();
 		if(reverse) x = 1-x;
 		for (int i = 0; i < updaters.length; i++) {
 			updaters[i].iupdate(x);
@@ -61,6 +110,15 @@ public class Animation {
 			notifyListeners(AnimationEvent.FIN_FOWARD);
 		}
 	}
+
+	/**1 means it's approching the end of this stage, if playing backwards 1 is the start*/
+	public synchronized float getProgress() {
+		return Math.min(1, (System.currentTimeMillis()-startTime) / (float)duration );
+	}
+
+	public synchronized boolean isReverse() {
+		return reverse;
+	}
 	
 	public enum TimeUnit{
 		MILLISECONDS(1),
@@ -72,10 +130,11 @@ public class Animation {
 			this.mult = mult;
 		}
 	}
-	
+
 	public enum AnimationEvent{
 		START_FORWARD, FIN_FOWARD,
-		START_REVERSE, FIN_REVERSE;
+		START_REVERSE, FIN_REVERSE,
+		PAUSE, RESUME;
 		public boolean isFow() {
 			return this.equals(FIN_FOWARD) || this.equals(START_FORWARD);
 		}
@@ -101,7 +160,7 @@ public class Animation {
 			return this.equals(FIN_REVERSE);
 		}
 	}
-	
+
 	abstract public static class Updater<S> {
 		final S start, stop;
 		Interpolator<? extends S> interpolator;
@@ -115,13 +174,40 @@ public class Animation {
 		}
 		abstract public void update(S p);
 	}
-	
+
 	abstract public static class Interpolator<S>{
-		abstract public S interpolate(float x);
+		/**Avoid this for starting and stopping animations, fine in the middle*/
+		public static final Interpolator<Float> LINEAR = new Interpolator<Float>() {
+			@Override public Float interpolate(float x) {
+				return x;
+			}};
+			public static final Interpolator<Float> SIGMOID = new Interpolator<Float>() {
+				@Override public Float interpolate(float x) {
+					return (float) (1/(1+Math.pow(Math.E, -((x-.5)*16))));
+				}
+			};
+
+			public static Interpolator<Float> accelerate(float exitVelocity){
+				return new Interpolator<Float>() {@Override
+					public Float interpolate(float x) {
+					return (float) Math.pow(x, exitVelocity); //Calculus!
+				}
+				};
+			}
+			public static Interpolator<Float> decelerate(float entryVelocity){
+				return new Interpolator<Float>() {@Override
+					public Float interpolate(float x) {
+					return (float) Math.pow(1-x, entryVelocity); //Calculus!
+				}
+				};
+			}
+
+			abstract public S interpolate(float x);
 	}
-	
+
 	@FunctionalInterface
 	abstract public interface AnimationEventHandler {
 		public void handleEvent(AnimationEvent event);
 	}
+
 }
